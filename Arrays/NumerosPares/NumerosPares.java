32e8769a1e73d1bb60af66b92e3b1fc6bd55dc80
Luccas Guimarães <contato.luccasguimaraes@gmail.com>
2022-06-29 11:56:24 -0300

    Desafios solucionados

---
+++ Arrays/NumerosPares/NumerosPares.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class NumerosPares {
+    public static void main(String args[]) {
+      Scanner scan = new Scanner(System.in);
+      
+      int[] numbers = new int[5];
+      int testNumber = 0;
+      
+      for(int i = 0; i < 5; i++){
+          testNumber = scan.nextInt();
+          if(testNumber % 2 != 0){
+              System.out.println(testNumber + " não pode ser salvo! Trata-se de um número ímpar.");
+              continue;
+            }
+            numbers[i] = testNumber;
+            System.out.println("O número " + numbers[i] + " foi salvo com sucesso!");
+        }  
+    }
+}
